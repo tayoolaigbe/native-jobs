e9109548b4a41b3f25c1d395da380af7364a3104
tayoolaigbe <tayoolaigbe@outlook.com>
2023-06-10 18:14:17 +0100

    Fixed the header error

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react';
+import {
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	Image,
+	FlatList,
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
+import styles from './welcome.style';
 
 const Welcome = () => {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+	return (
+		<View>
+			<Text>Welcome</Text>
+		</View>
+	);
+};
 
-export default Welcome
+export default Welcome;
