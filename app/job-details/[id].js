15df44016f929c9df0cd96ae7fa725957e979c05
tayoolaigbe <tayoolaigbe@outlook.com>
2023-06-11 00:52:37 +0100

    Fixed the JobDetails forwardRefimport error

---
+++ app/job-details/[id].js
@@ -0,0 +1,67 @@
+import { Stack, useRouter, useSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+import {
+	View,
+	Text,
+	SafeAreaView,
+	ScrollView,
+	ActivityIndicator,
+	RefreshControl,
+} from 'react-native';
+
+import {
+	Company,
+	JobAbout,
+	JobFooter,
+	JobTabs,
+	ScreenHeaderBtn,
+	Specifics,
+} from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hooks/useFetch';
+
+const JobDetails = () => {
+	const params = useSearchParams();
+	const router = useRouter();
+
+	const { data, isLoading, error, refetch } = useFetch('job-details', {
+		job_id: params.id,
+	});
+
+	const [refreshing, setRefreshing] = useState(false);
+
+	const onRefresh = () => {};
+
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+			<Stack.Screen
+				options={{
+					headerStyle: { backgroundColor: COLORS.lightWhite },
+					headerShadowVisible: false,
+					headerBackVisible: false,
+					headerLeft: () => (
+						<ScreenHeaderBtn
+							iconUrl={icons.left}
+							dimension="60%"
+							handlePress={() => router.back()}
+						/>
+					),
+					headerRight: () => (
+						<ScreenHeaderBtn iconUrl={icons.share} dimension="60%" />
+					),
+					headerTitle: '',
+				}}
+			/>
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				refreshControl={
+					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+				}
+			>
+				<Text>Hi</Text>
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
+
+export default JobDetails;
