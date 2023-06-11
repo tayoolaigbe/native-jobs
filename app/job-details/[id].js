17060c0990e245716d7bdad9bf4dd2a3b0a01768
tayoolaigbe <tayoolaigbe@outlook.com>
2023-06-11 13:33:59 +0100

    Completed the body of the JobDetails component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -20,6 +20,8 @@ import {
 import { COLORS, icons, SIZES } from '../../constants';
 import useFetch from '../../hooks/useFetch';
 
+const tabs = ['About', 'Qualifications', 'Responsibilities'];
+
 const JobDetails = () => {
 	const params = useSearchParams();
 	const router = useRouter();
@@ -29,8 +31,37 @@ const JobDetails = () => {
 	});
 
 	const [refreshing, setRefreshing] = useState(false);
+	const [activeTab, setActiveTab] = useState(tabs[0]);
 
 	const onRefresh = () => {};
+
+	const displayTabContext = () => {
+		switch (activeTab) {
+			case 'Qualifications':
+				return (
+					<Specifics
+						title="Qualifications"
+						points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+					/>
+				);
+				break;
+			case 'About':
+				return (
+					<JobAbout info={data[0].job_description ?? 'No data provided'} />
+				);
+				break;
+			case 'Responsibilities':
+				return (
+					<Specifics
+						title="Responsibilities"
+						points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+					/>
+				);
+
+			default:
+				break;
+		}
+	};
 
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -58,7 +89,28 @@ const JobDetails = () => {
 					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
 				}
 			>
-				<Text>Hi</Text>
+				{isLoading ? (
+					<ActivityIndicator size="large" color={COLORS.primary} />
+				) : error ? (
+					<Text>Something went wrong!</Text>
+				) : data.length === 0 ? (
+					<Text>No Data</Text>
+				) : (
+					<View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+						<Company
+							companyLogo={data[0].employer_logo}
+							jobTitle={data[0].job_title}
+							companyName={data[0].employer_name}
+							location={data[0].job_country}
+						/>
+						<JobTabs
+							tabs={tabs}
+							activeTab={activeTab}
+							setActiveTab={setActiveTab}
+						/>
+						{displayTabContext()}
+					</View>
+				)}
 			</ScrollView>
 		</SafeAreaView>
 	);
